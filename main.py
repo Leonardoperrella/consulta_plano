45b3508dcb426fab75801a4a1199d88870ff8a7d
C1321648 Leonardo Augusto Perrella <c1321648@interno.bb.com.br>
2025-04-12 09:52:17 -0300

    responsivo

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
-
+import io
+import pandas as pd
+import numpy as np
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(
     __name__,
@@ -7,10 +9,89 @@ app = Flask(
     static_url_path='/static',
     template_folder='src/templates')
 
+csv_data = []
+excel_data = []
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/upload_excel", methods=["POST"])
+def upload_excel():
+    try:
+        print("Received upload_excel request")
+        if "file" not in request.files:
+            return jsonify({"error": "No file part"}), 400
+        file = request.files["file"]
+        if file.filename == "":
+            return jsonify({"error": "No selected file"}), 400
+
+        if file and file.filename.endswith((".xlsx", ".xls")):
+            print(f"Processing file: {file.filename}")
+            df = pd.read_excel(file, header=6)
+            df.columns = ['cod_benef', 'nome_benef', 'cod_depend', 'nome_depend', 'matricula',
+                          'tipo', 'sexo', 'dt_nasc', 'idade', 'inicio_vigencia', 'fim_vigencia', 'plano']
+
+            # Convert date columns to strings in isoformat
+            date_columns = ['dt_nasc', 'inicio_vigencia', 'fim_vigencia']
+            for col in date_columns:
+                if col in df.columns:
+                    try:
+                        df[col] = pd.to_datetime(df[col], errors='coerce')
+                        df[col] = df[col].dt.strftime('%Y-%m-%d').fillna('')
+                    except Exception as error:
+                        print(f"Error processing column {col}: {error}")
+            df = df.replace({pd.NaT: '', np.nan: ''})
+            data = df.to_dict(orient="records")
+            global excel_data
+            excel_data = data
+            return jsonify(data)
+
+        else:
+            return jsonify({"error": "Invalid file format. Only Excel files are allowed."}), 400
+    except Exception as e:
+        print(e)
+        return (
+            jsonify({"error": "An error occurred while processing the file."}),
+            500,
+        )
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    term = request.args.get('term', '').lower()
+
+    if not csv_data and not excel_data:
+        return jsonify({'message': 'No data available. Please upload a CSV or Excel file.'})
+
+    results = []
+
+    # Search in CSV data if available
+    if csv_data:
+        results.extend([item for item in csv_data if
+                        term in str(item.get('Matrícula', '')).lower() or
+                        term in str(item.get('Nome Beneficiário', '')).lower()])
+
+    # Search in Excel data if available
+    if excel_data:
+        results.extend([item for item in excel_data if
+                        term in str(item.get('matricula', '')).lower() or
+                        term in str(item.get('nome_benef', '')).lower()])
+
+    # Remove duplicates based on the combined fields if any exist
+    unique_results = {}
+    for item in results:
+        if isinstance(item, dict):  # Check if item is a dictionary
+            key = tuple(item.items())
+            if key not in unique_results:
+                unique_results[key] = item
+        else:
+            # Log non-dictionary items
+            print(f"Skipping non-dictionary item: {item}")
+
+    return jsonify(list(unique_results.values()))
 
 
 def main():
--- src/static/script.js
+++ src/static/script.js
@@ -1,9 +1,11 @@
 let csvData = []; // Global variable to store CSV data
 
 const fileInput = document.getElementById('csvFileInput');
+const excelFileInput = document.getElementById('excelFileInput');
 const searchInput = document.getElementById('searchInput');
 const fileNameDisplay = document.getElementById('fileName'); // Add this line
 
+excelFileInput.addEventListener('change', handleExcelFileSelect);
 searchInput.addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
         searchData();
@@ -44,22 +46,86 @@ function handleFileSelect(event) {
     reader.readAsText(file);
 }
 
+async function handleExcelFileSelect(event) {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append('file', file); // 'file' é o nome do campo que o servidor espera
+    try {
+        document.getElementById('loading').style.display = 'block'; // Show loading
+        const response = await fetch('/upload_excel', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data)
+            if (data.error) {
+                alert('Erro ao processar o arquivo: ' + data.error);
+            } else {
+                displayResults(data); // Chama displayResults com os dados recebidos
+            }
+        } else {
+            alert('Erro response ao enviar o arquivo para o servidor.');
+        }
+    } catch (error) {
+        console.error('Error uploading file:', error);
+        alert('Erro ao enviar o arquivo para o servidor.');
+    } finally {
+        document.getElementById('loading').style.display = 'none'; // Hide loading
+    }
+}
+
+function displayResults(results) {
+    const resultsBody = document.getElementById("resultsBody");
+    resultsBody.innerHTML = '';
+    if (!results || results.length === 0) {
+        const row = document.createElement('tr');
+        row.innerHTML = '<td colspan="12">No results found</td>';
+        resultsBody.appendChild(row);
+    }
+    results.forEach(item => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${item.cod_benef || ''}</td>
+            <td>${item.nome_benef || ''}</td>
+            <td>${item.cod_depend || ''}</td>
+            <td>${item.nome_depend || ''}</td>
+            <td>${item.matricula || ''}</td>
+            <td>${item.tipo || ''}</td>
+            <td>${item.sexo || ''}</td>
+            <td>${item.dt_nasc || ''}</td>
+            <td>${item.idade || ''}</td>
+            <td>${item.inicio_vigencia || ''}</td>
+            <td>${item.fim_vigencia || ''}</td>
+            <td>${item.plano || ''}</td>
+        `;
+        resultsBody.appendChild(row);
+    });
+    document.getElementById('loading').style.display = 'none';
+}
+
 async function searchData() {
     document.getElementById('loading').style.display = 'block';
-
-    if (!csvData || csvData.length === 0) {
-        alert('Please select a CSV file first.');
-        document.getElementById('loading').style.display = 'none';
-        return;
-    }
     const searchTerm = document.getElementById("searchInput").value.trim().toLowerCase();
-    const results = csvData.filter(item => {
-        const matricula = item['Matrícula'] ? item['Matrícula'].toLowerCase() : '';
-        const nome = item['Nome Beneficiário'] ? item['Nome Beneficiário'].toLowerCase() : '';
-        return matricula.includes(searchTerm) || nome.includes(searchTerm);
-    });
-
-    displayResults(results);
+    try {
+        const response = await fetch(`/search?term=${searchTerm}`, {
+            method: 'GET',
+        });
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            alert('Erro ao pesquisar no servidor.');
+            document.getElementById('loading').style.display = 'none';
+        }
+    } catch (error) {
+        console.error('Error searching data:', error);
+        alert('Erro ao pesquisar no servidor.');
+        document.getElementById('loading').style.display = 'none';
+    }
 }
 
 function formatScientificString(str) {
@@ -74,32 +140,4 @@ function formatScientificString(str) {
 
     // Verifica se é um número válido e retorna formatado
     return isNaN(num) ? str : num.toLocaleString('pt-BR');
-}
-
-function displayResults(results) {
-    const resultsBody = document.getElementById("resultsBody");
-    resultsBody.innerHTML = '';
-
-    results.forEach(item => {
-        const row = document.createElement('tr');
-        const formattedBeneficiario = formatScientificString(item['Código do Beneficiário']);
-        const formattedDependente = formatScientificString(item['Código do Dependente']);
-
-        row.innerHTML = `
-            <td>${formattedBeneficiario || ''}</td>
-            <td>${item['Nome Beneficiário'] || ''}</td>
-            <td>${formattedDependente || ''}</td>
-            <td>${item['Nome Dependente'] || ''}</td>
-            <td>${item['Matrícula'] || ''}</td>
-            <td>${item['Tipo'] || ''}</td>
-            <td>${item['Sexo'] || ''}</td>
-            <td>${item['Dt. Nascimento'] || ''}</td>
-            <td>${item['Idade'] || ''}</td>
-            <td>${item['Início Vigência'] || ''}</td>
-            <td>${item['Fim Vigência'] || ''}</td>
-            <td>${item['Plano'] || ''}</td>
-        `;
-        resultsBody.appendChild(row);
-    });
-    document.getElementById('loading').style.display = 'none';
 }
